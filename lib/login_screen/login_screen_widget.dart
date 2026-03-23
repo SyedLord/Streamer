0ad834f456c3fd6a39ad9ed1f34887884b342dbf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-23 17:27:12 +0000

    Login Issue Resolved

--- lib/login_screen/login_screen_widget.dart
+++ lib/login_screen/login_screen_widget.dart
@@ -35,7 +35,7 @@ class _LoginScreenWidgetState extends State<LoginScreenWidget> {
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
       if (FFAppState().isLoggedIn == true) {
-        context.pushNamed(CreatorStudioHubWidget.routeName);
+        context.goNamed(CreatorStudioHubWidget.routeName);
       }
     });
   }
