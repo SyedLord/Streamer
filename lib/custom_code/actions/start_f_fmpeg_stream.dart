79fdb8bdaa7a403b7370836eafbcfe641ebf11d8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-24 06:32:18 +0000

    code fix

--- lib/custom_code/actions/start_f_fmpeg_stream.dart
+++ lib/custom_code/actions/start_f_fmpeg_stream.dart
@@ -30,9 +30,15 @@ Future startFFmpegStream(
   // String command = "-re -i \"$videoPath\" -c copy -f flv \"$fullRtmpUrl\"";
   // The Pure PS5 Direct Copy Command (Python Exact Match)
   // YouTube Optimized Command (Mobile Network Stutter Fix)
+  // String command = "-re -i \"$videoPath\" "
+  //     "-c:v libx264 -preset ultrafast "
+  //     "-b:v 3000k -maxrate 3000k -bufsize 6000k "
+  //     "-g 60 -c:a aac -b:a 128k -ar 44100 "
+  //     "-f flv \"$fullRtmpUrl\"";
+  // YouTube 1080p High-Quality Command
   String command = "-re -i \"$videoPath\" "
       "-c:v libx264 -preset ultrafast "
-      "-b:v 3000k -maxrate 3000k -bufsize 6000k "
+      "-b:v 6800k -maxrate 6800k -bufsize 13600k "
       "-g 60 -c:a aac -b:a 128k -ar 44100 "
       "-f flv \"$fullRtmpUrl\"";
 
