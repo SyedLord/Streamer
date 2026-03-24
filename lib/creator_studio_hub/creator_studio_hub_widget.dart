4fc969a3daceff238881488ecbf357af48cd4e74
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-24 06:08:27 +0000

    Updating to latest FlutterFlow output.

--- lib/creator_studio_hub/creator_studio_hub_widget.dart
+++ lib/creator_studio_hub/creator_studio_hub_widget.dart
@@ -115,6 +115,7 @@ class _CreatorStudioHubWidgetState extends State<CreatorStudioHubWidget> {
                               FFAppState().globalRtmp = '';
                               FFAppState().globalStreamKey = '';
                               FFAppState().isLoggedIn = false;
+                              FFAppState().selectedVideoPath = '';
                               safeSetState(() {});
 
                               context.goNamed(LoginScreenWidget.routeName);
