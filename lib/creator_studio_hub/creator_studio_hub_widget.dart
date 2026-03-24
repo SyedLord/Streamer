5ea9d848ca4d5713e1dd513cbee5fabc262cefd6
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-24 07:42:29 +0000

    Updating to latest FlutterFlow output.

--- lib/creator_studio_hub/creator_studio_hub_widget.dart
+++ lib/creator_studio_hub/creator_studio_hub_widget.dart
@@ -748,6 +748,21 @@ class _CreatorStudioHubWidgetState extends State<CreatorStudioHubWidget> {
 
                                 context.pushNamed(
                                     LiveBroadcastModeWidget.routeName);
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      'api not working',
+                                      style: TextStyle(
+                                        color: FlutterFlowTheme.of(context)
+                                            .primaryText,
+                                      ),
+                                    ),
+                                    duration: Duration(milliseconds: 4000),
+                                    backgroundColor:
+                                        FlutterFlowTheme.of(context).secondary,
+                                  ),
+                                );
                               }
                             } else {
                               ScaffoldMessenger.of(context).showSnackBar(
