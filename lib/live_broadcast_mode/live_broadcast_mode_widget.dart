c49ad5e30e1971010b885ea55174eae55469431a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-25 20:09:18 +0000

    Updating to latest FlutterFlow output.

--- lib/live_broadcast_mode/live_broadcast_mode_widget.dart
+++ lib/live_broadcast_mode/live_broadcast_mode_widget.dart
@@ -66,6 +66,7 @@ class _LiveBroadcastModeWidgetState extends State<LiveBroadcastModeWidget> {
             r'''$.viewers''',
           ).toString();
           safeSetState(() {});
+          _model.instantTimer?.cancel();
           _model.instantTimer2 = InstantTimer.periodic(
             duration: Duration(milliseconds: 1000),
             callback: (timer) async {
