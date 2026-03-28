a204f1e457a969f4129af060b68f94d66c588eca
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-28 11:35:06 +0000

    added toast message for select the video

--- lib/creator_studio_hub/creator_studio_hub_widget.dart
+++ lib/creator_studio_hub/creator_studio_hub_widget.dart
@@ -1514,19 +1514,9 @@ class _CreatorStudioHubWidgetState extends State<CreatorStudioHubWidget> {
                                   context.goNamed(LoginScreenWidget.routeName);
                                 }
                               } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                      'Select the Video !',
-                                      style: TextStyle(
-                                        color: FlutterFlowTheme.of(context)
-                                            .primaryText,
-                                      ),
-                                    ),
-                                    duration: Duration(milliseconds: 4000),
-                                    backgroundColor:
-                                        FlutterFlowTheme.of(context).error,
-                                  ),
+                                await actions.showNativeToast(
+                                  'Select the Video !',
+                                  true,
                                 );
                               }
                             }
